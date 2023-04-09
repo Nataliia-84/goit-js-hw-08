d8ffd9057a4945e5cb87678a562ffe38f8fcc0d1
Nataliia Korotych <natalya.cherlenyuk@gmail.com>
2023-04-09 10:50:48 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,47 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+console.log(Player)
+const iframe=document.querySelector('#vimeo-player')
+console.log(iframe)
+
+const KEY_LOCALE="videoplayer-current-time"
+const player = new Player(iframe);
+
+
+
+
+
+
+
+
+player.on('timeupdate', throttle(function(data) {
+    
+    {
+        duration: 61.857
+        percent: 0.049
+        seconds: 3.034
+    }
+data.seconds;
+
+console.log(data.seconds)
+localStorage.setItem('KEY_LOCALE',JSON.stringify(data.seconds))
+
+}),1000);
+
+let currentTime =Number(localStorage.getItem(JSON.parse('KEY_LOCALE')))
+console.log(currentTime)
+
+player.setCurrentTime(currentTime).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -8,7 +8,7 @@ console.log(formEl);
 const filds = {};
 
 // викликаю ініціалізацію форми
-initForm()
+// initForm()
 
 
 formEl.addEventListener('input', throttle(onClickInput, 500));
@@ -20,7 +20,7 @@ function onClickInput(event){
     filds[event.target.name]=event.target.value;
     console.log(filds)
     localStorage.setItem('feedback-form-state', JSON.stringify(filds));
-    
+    console.log(filds)
 };
 
 
@@ -29,35 +29,35 @@ function onClickInput(event){
 // у формі ключа записую значення
 
 
-function initForm(){
-    let formsFild = localStorage.getItem('feedback-form-state')
-    // console.log(formsFild)
-    if(formsFild){
-        formsFild=JSON.parse(formsFild);
-        // console.log(formsFild);
-        Object.entries(formsFild).forEach(([name,value])=>{
-            // filds[name]=value;
-           formEl.elements[name].value=value})
+// function initForm(){
+//     let formsFild = localStorage.getItem('feedback-form-state')
+//     console.log(formsFild)
+//     if(formsFild){
+//         formsFild=JSON.parse(formsFild);
+//         // console.log(formsFild);
+//         Object.entries(formsFild).forEach(([name,value])=>{
+//             // filds[name]=value;
+//            formEl.elements[name].value=value})
       
-}
+// }
            
 
 
-}
+// }
 
-formEl.addEventListener('submit',onClickForm)
+// formEl.addEventListener('submit',onClickForm)
 
-function onClickForm(event){
-    event.preventDefault();
+// function onClickForm(event){
+//     event.preventDefault();
 
-    if(formEl.elements.email.value.trim()==="" || formEl.elements.message.value.trim()===""){
-        alert(" Всі поля потрібно заповнити")
+//     if(formEl.elements.email.value.trim()==="" || formEl.elements.message.value.trim()===""){
+//         alert(" Всі поля потрібно заповнити")
         
-    } 
+//     } 
     
-       formEl.elements.email.value="";
-        formEl.elements.message.value="";
-        localStorage.removeItem('feedback-form-state')
+//        formEl.elements.email.value="";
+//         formEl.elements.message.value="";
+//         localStorage.removeItem('feedback-form-state')
         
-};
+// };
 
