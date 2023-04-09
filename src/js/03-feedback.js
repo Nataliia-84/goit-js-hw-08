ff67e76c04e4e2f8b5db7d86548fb960490988a5
Nataliia Korotych <natalya.cherlenyuk@gmail.com>
2023-04-09 20:57:02 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -25,39 +25,39 @@ function onClickInput(event){
 
 
 // виконую ініціалізацію форми, з localStorage беру записані дані
-//і якщо ці дані там є розпаковую дані і витягую дані, далі,
+// і якщо ці дані там є розпаковую дані і витягую дані, далі,
 // у формі ключа записую значення
 
 
-// function initForm(){
-//     let formsFild = localStorage.getItem('feedback-form-state')
-//     console.log(formsFild)
-//     if(formsFild){
-//         formsFild=JSON.parse(formsFild);
-//         // console.log(formsFild);
-//         Object.entries(formsFild).forEach(([name,value])=>{
-//             // filds[name]=value;
-//            formEl.elements[name].value=value})
+function initForm(){
+    let formsFild = localStorage.getItem('feedback-form-state')
+    console.log(formsFild)
+    if(formsFild){
+        formsFild=JSON.parse(formsFild);
+        // console.log(formsFild);
+        Object.entries(formsFild).forEach(([name,value])=>{
+            // filds[name]=value;
+           formEl.elements[name].value=value})
       
-// }
+}
            
 
 
-// }
+}
 
-// formEl.addEventListener('submit',onClickForm)
+formEl.addEventListener('submit',onClickForm)
 
-// function onClickForm(event){
-//     event.preventDefault();
+function onClickForm(event){
+    event.preventDefault();
 
-//     if(formEl.elements.email.value.trim()==="" || formEl.elements.message.value.trim()===""){
-//         alert(" Всі поля потрібно заповнити")
+    if(formEl.elements.email.value.trim()==="" || formEl.elements.message.value.trim()===""){
+        alert(" Всі поля потрібно заповнити")
         
-//     } 
+    } 
     
-//        formEl.elements.email.value="";
-//         formEl.elements.message.value="";
-//         localStorage.removeItem('feedback-form-state')
+       formEl.elements.email.value="";
+        formEl.elements.message.value="";
+        localStorage.removeItem('feedback-form-state')
         
-// };
+};
 
